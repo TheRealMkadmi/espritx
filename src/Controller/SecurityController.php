318358a197821fa396be92c199a422ebede97c1c
HexCrunch <wahib.mkadmi@esprit.tn>
2022-02-15 16:32:49 +0100

    Fixed fullLayoutMaster.html.twig

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,28 +9,28 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('home');
-        }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+  /**
+   * @Route("/login", name="login")
+   */
+  public function login(AuthenticationUtils $authenticationUtils): Response
+  {
+    if ($this->getUser()) {
+      return $this->redirectToRoute('dashboard-analytics');
     }
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastUsername = $authenticationUtils->getLastUsername();
+    return $this->render('views/content/authentication/auth-login-cover.html.twig',
+      [
+        'last_username' => $lastUsername,
+        'error' => $error
+      ]);
+  }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout(): void
-    {
-        throw new \LogicException('Method should have been intercepted by the firewall.');
-    }
+  /**
+   * @Route("/logout", name="app_logout")
+   */
+  public function logout(): void
+  {
+    throw new \LogicException('Method should have been intercepted by the firewall.');
+  }
 }
