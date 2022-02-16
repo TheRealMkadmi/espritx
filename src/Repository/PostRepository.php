9bb7af6426068dd514be4a9003ef31e7328ba2b7
Mouna-Amdouni <56323930+Mouna-Amdouni@users.noreply.github.com>
2022-02-16 21:19:25 +0100

    Add files via upload

---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Post|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Post|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Post[]    findAll()
+ * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    // /**
+    //  * @return Post[] Returns an array of Post objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Post
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+    public function changeValidite(Post $post){
+        $em=$this->getEntityManager();
+        if ($post->isValid())
+            $post->setIsValid(false);
+        else
+            $post->setIsValid(true);
+        $em->persist($post);
+        $em->flush();
+        return $post;
+    }
+    public function changeDelete(Post $post){
+        $em=$this->getEntityManager();
+        if ($post->getIsDeleted())
+            $post->setIsDeleted(false);
+        else
+            $post->setIsDeleted(true);
+        $em->persist($post);
+        $em->flush();
+        return $post;
+    }
+
+}
