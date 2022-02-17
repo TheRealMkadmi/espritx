91e15ebcbcda33cd26c7ea592c30e57ab1d16f2b
Hedi-2P1 <kefi.mohamedhedi@esprit.tn>
2022-02-17 16:22:07 +0100

    Added basic CRUD for Service type

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Service;
 use App\Repository\ServiceRepository;
 use App\Form\ServiceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,28 +15,45 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route ("/service")
  */
- class ServiceController extends AbstractController
+class ServiceController extends AbstractController
 {
     /**
      * @Route("/", name="service")
      */
-    public function index(): Response
+    public function Services(EntityManagerInterface $em): Response
     {
-        return $this->render('service/index.html.twig', [
-            'controller_name' => 'ServiceController',
+
+        $services = $em->getRepository(Service::class)->findAll();
+        return $this->render('views/content/apps/administrativeService/serviceAffiche.html.twig',[
+            'pagination'=>$services
         ]);
     }
 
     /**
      * @Route("/add", name="serivceAdd")
      */
-    public function addService(Request $request):Response
+    public function addService(Request $request, EntityManagerInterface $em): Response
     {
         $ser = new Service();
-        $f = $this->createForm(ServiceType::class,$ser);
+        $f = $this->createForm(ServiceType::class, $ser);
         $f->handleRequest($request);
-        return $this->render('service/AddService.html.twig', [
-            'form'=>$f->createView(),
+        if ($f->isSubmitted() && $f->isValid()) {
+            $em->persist($ser);
+            $em->flush();
+            return $this->redirectToRoute('service', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('views/content/apps/administrativeService/app-service-form.html.twig', [
+            'form' => $f->createView(),
         ]);
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @Route ("/aff", name="AffAll")
+     */
+    public function affServices(EntityManagerInterface $em): Response
+    {
+
     }
 }
--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -2,34 +2,40 @@
 
 namespace App\Form;
 
-use App\Entity\FormFields;
 use App\Entity\Group;
 use App\Entity\Service;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvents;
 
 class ServiceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
             ->add('Name')
             ->add('Responsible',
                 EntityType::class,
-                ['class' => Group::class,
-                    'choice_label' => 'display_name',
-                    'placeholder' => 'Sélectionner le départment responsable'])
+                ['class'=>Group::class,
+                    'choice_label'=>'display_name',
+                    'placeholder'=>'Sélectionner le départment responsable'])
             ->add('Recipient',
                 EntityType::class,
-                ['class' => Group::class,
-                    'choice_label' => 'display_name',
-                    'expanded' => true,
-                    'multiple' => true])
-            ->add('Ajouter', SubmitType::class);
+                ['class'=>Group::class,
+                    'choice_label'=>'display_name',
+                    'expanded'=>true,
+                    'multiple'=>true])
+            ->add('Ajouter',SubmitType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
