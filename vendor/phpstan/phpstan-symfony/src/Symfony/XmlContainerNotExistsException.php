2e4cf5b1cf4652c91e60c82cadb3f2a29fe79f31
Mouna-Amdouni <mounaamdouni213@gmail.com>
2022-02-19 14:39:06 +0100

    Cleaning up work tree

--- vendor/phpstan/phpstan-symfony/src/Symfony/XmlContainerNotExistsException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace PHPStan\Symfony;
-
-final class XmlContainerNotExistsException extends \InvalidArgumentException
-{
-
-}
--- vendor/tetranz/select2entity-bundle/TetranzSelect2EntityBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tetranz\Select2EntityBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TetranzSelect2EntityBundle extends Bundle
-{
-}
