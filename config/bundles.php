64664a43fe107705dcb10600ff437b497032105d
HexCrunch <wahib.mkadmi@esprit.tn>
2022-02-27 22:58:35 +0100

    Added support for basic authorization

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
