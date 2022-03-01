025cc388f858f230f0385e6c10deacfd125e3713
snous <nesrine.snoussi@esprit.tn>
2022-03-01 20:27:56 +0100

    Blog_Post02

--- src/Controller/PostCategoryController.php
+++ src/Controller/PostCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,11 @@ class PostCategoryController extends AbstractController
     /**
      * @Route("/post/category", name="app_post_category")
      */
-    public function index(): Response
+    public function index(PostCategoryRepository $postCategoryRepository): Response
     {
+        $posts = $postCategoryRepository->findAll(); //afficher les données stocké de post category
         return $this->render('post_category/index.html.twig', [
+            'posts' => $posts,
             'controller_name' => 'PostCategoryController',
         ]);
     }
--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BlogPostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BlogPostRepository::class)
