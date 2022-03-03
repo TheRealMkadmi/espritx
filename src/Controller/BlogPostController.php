e9fbe11a100c470f88721b189d43afd93710d313
snous <nesrine.snoussi@esprit.tn>
2022-03-03 22:14:31 +0100

    Forum_EspritX

--- src/Controller/BlogPostController.php
+++ src/Controller/BlogPostController.php
@@ -16,7 +16,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/** @Route("/forum") */
 class BlogPostController extends AbstractController
 {
     /**
