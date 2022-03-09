eafdacc28d4b0c15d3993669d185762312cb4d93
MoetezKhemissi <khemissimoetez@gmail.com>
2022-03-10 00:34:38 +0100

    Fixed Template

--- config/bundles.php
+++ config/bundles.php
@@ -31,5 +31,4 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
