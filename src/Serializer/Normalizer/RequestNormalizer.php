ee6c154b0b01b096dbc5e82b4358fd9fdd770df1
HexCrunch <wahib.mkadmi@esprit.tn>
2022-03-26 12:27:18 +0100

    Multiple Bugfixes

--- src/Serializer/Normalizer/RequestNormalizer.php
+++ src/Serializer/Normalizer/RequestNormalizer.php
@@ -2,6 +2,7 @@
 
 namespace App\Serializer\Normalizer;
 
+use App\Entity\Group;
 use App\Entity\ServiceRequest;
 use App\Entity\Service;
 use App\Entity\User;
@@ -13,45 +14,48 @@ use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 class RequestNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
 {
 
-    public function __construct(private ObjectNormalizer $normalizer,
-                                private UploaderHelper   $helper,
-                                private UserNormalizer $userNormalizer)
-    {
-    }
+  public function __construct(private ObjectNormalizer  $normalizer,
+                              private UploaderHelper    $helper,
+                              private UserNormalizer    $userNormalizer)
+  {
+  }
 
-    /**
-     * @param ServiceRequest $request
-     * @param $format
-     * @param array $context
-     * @return array
-     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
-     */
-    public function normalize($request, $format = null, array $context = []): array
-    {
-        return [
-            'id' => $request->getId(),
-            'CreatedAt'=>$request->getCreatedAt(),
-            'UpdatedAt'=>$request->getUpdatedAt(),
-            'RespondedAt' => $request->getRespondedAt(),
-            'Title' => $request->getTitle(),
-            'Description' => $request->getDescription(),
-            'Type' => $request->getType()->getName(),
-            'Status' => $request->getStatus(),
-            'Response' => $request->getRequestResponse(),
-            'Picture' => $request->getPicture(),
-            'Attachements' => $request->getAttachements(),
-            'Email' => $request->getEmail(),
-            'Requester' => $this->userNormalizer->normalize($request->getRequester()),
-        ];
-    }
+  /**
+   * @param ServiceRequest $request
+   * @param $format
+   * @param array $context
+   * @return array
+   * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+   */
+  public function normalize($request, $format = null, array $context = []): array
+  {
+    return [
+      'id' => $request->getId(),
+      'CreatedAt' => $request->getCreatedAt()->format('Y-m-d\TH:m:s.u'),
+      'UpdatedAt' => $request->getUpdatedAt()->format('Y-m-d\TH:m:s.u'),
+      'RespondedAt' => $request->getRespondedAt()?->format('Y-m-d\TH:m:s.u'),
+      'Title' => $request->getTitle(),
+      'Description' => $request->getDescription(),
+      'Type' => [
+        'id' => $request->getType()->getId(),
+        'Name' => $request->getType()->getName()
+      ],
+      'Status' => $request->getStatus(),
+      'Response' => $request->getRequestResponse(),
+      'Picture' => $request->getPicture(),
+      'Attachements' => $request->getAttachements(),
+      'Email' => $request->getEmail(),
+      'Requester' => $this->userNormalizer->normalize($request->getRequester()),
+    ];
+  }
 
-    public function supportsNormalization($data, $format = null): bool
-    {
-        return $data instanceof \App\Entity\ServiceRequest;
-    }
+  public function supportsNormalization($data, $format = null): bool
+  {
+    return $data instanceof \App\Entity\ServiceRequest;
+  }
 
-    public function hasCacheableSupportsMethod(): bool
-    {
-        return true;
-    }
+  public function hasCacheableSupportsMethod(): bool
+  {
+    return true;
+  }
 }
