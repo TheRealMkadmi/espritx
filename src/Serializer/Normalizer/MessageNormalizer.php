5b5e0865dff4b8b014f4cabb0e7bf7ae9e3b71f0
HexCrunch <wahib.mkadmi@esprit.tn>
2022-05-16 01:41:25 +0100

    Kil gamla ybakech...

--- src/Serializer/Normalizer/MessageNormalizer.php
+++ src/Serializer/Normalizer/MessageNormalizer.php
@@ -15,45 +15,45 @@ use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class MessageNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
 {
-    public function __construct(private HttpFoundationExtension $httpFoundationExtension, private UploaderHelper $helper,)
-    {
-    }
+  public function __construct(private HttpFoundationExtension $httpFoundationExtension, private UploaderHelper $helper,)
+  {
+  }
 
-    /**
-     * @param Message $object
-     * @param $format
-     * @param array $context
-     * @return array
-     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
-     */
-    public function normalize($object, $format = null, array $context = []): array
-    {
-        return [
-            'id' => $object->getId(),
-            'content'=>$object->getContent(),
-            'author' => [
-                'id' => $object->getAuthor()->getId(),
-                'first_name' => $object->getAuthor()->getFirstName(),
-                'last_name' => $object->getAuthor()->getLastName(),
-                'email' => $object->getAuthor()->getEmail(),
-                'class' => $object->getAuthor()->getClass(),
-                'userStatus' => $object->getAuthor()->getUserStatus(),
-                'avatarFile' => ($object->getAuthor()->getAvatar()?->getName() !== null) ? $this->httpFoundationExtension->generateAbsoluteUrl($this->helper->asset($object->getAuthor(), "avatarFile")) : null,
-                'identityType' => $object->getAuthor()->getIdentityType(),
-                'identityDocumentNumber' => $object->getAuthor()->getIdentityDocumentNumber(),
-                'phoneNumber' => $object->getAuthor()->getPhoneNumber(),
-                'plainPassword' => $object->getAuthor()->getPlainPassword()
-            ],'Conversationid'=>$object->getChannel()->getId(),
-        ];
-    }
+  /**
+   * @param Message $object
+   * @param $format
+   * @param array $context
+   * @return array
+   * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+   */
+  public function normalize($object, $format = null, array $context = []): array
+  {
+    return [
+      'id' => $object->getId(),
+      'content' => $object->getContent(),
+      'author' => [
+        'id' => $object->getAuthor()->getId(),
+        'first_name' => $object->getAuthor()->getFirstName(),
+        'last_name' => $object->getAuthor()->getLastName(),
+        'email' => $object->getAuthor()->getEmail(),
+        'class' => $object->getAuthor()->getClass(),
+        'userStatus' => $object->getAuthor()->getUserStatus(),
+        'avatarFile' => ($object->getAuthor()->getAvatar()?->getName() !== null) ? $this->httpFoundationExtension->generateAbsoluteUrl($this->helper->asset($object->getAuthor(), "avatarFile")) : null,
+        'identityType' => $object->getAuthor()->getIdentityType(),
+        'identityDocumentNumber' => $object->getAuthor()->getIdentityDocumentNumber(),
+        'phoneNumber' => $object->getAuthor()->getPhoneNumber(),
+        'plainPassword' => $object->getAuthor()->getPlainPassword()
+      ], 'Conversationid' => $object->getChannel()->getId(),
+    ];
+  }
 
-    public function supportsNormalization($data, $format = null): bool
-    {
-        return $data instanceof \App\Entity\Message;
-    }
+  public function supportsNormalization($data, $format = null): bool
+  {
+    return $data instanceof \App\Entity\Message;
+  }
 
-    public function hasCacheableSupportsMethod(): bool
-    {
-        return true;
-    }
+  public function hasCacheableSupportsMethod(): bool
+  {
+    return true;
+  }
 }
